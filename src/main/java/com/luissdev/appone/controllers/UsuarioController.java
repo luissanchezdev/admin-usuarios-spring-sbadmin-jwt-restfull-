c273ae5a4e90db2c19d200296fcf5a3097ca883c
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-12-30 20:32:57 -0500

    fix: added sb admin 2 template

--- src/main/java/com/luissdev/appone/controllers/UsuarioController.java
+++ src/main/java/com/luissdev/appone/controllers/UsuarioController.java
@@ -3,11 +3,13 @@ package com.luissdev.appone.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class UsuarioController {
 
     @RequestMapping( value = "prueba")
-    public String prueba() {
-        return "prueba";
+    public List<String> prueba() {
+        return  List.of("c++", "java", "js");
     }
 }
